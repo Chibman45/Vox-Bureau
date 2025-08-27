9e872cbc2ab1ff0c3a4fa772eaba5c7411cd54d7
Chibuzor collins <chibuzorcollins6@gmail.com>
2025-08-27 12:44:41 +0000

    Add Vercel Analytics to the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/react';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: 'Vox-P',
@@ -11,24 +18,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params,
+  searchParams,
 }: Readonly<{
   children: React.ReactNode;
+  params: {};
+  searchParams: {};
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-body antialiased">
+    <html lang="en" className={inter.variable} suppressHydrationWarning>
+      <head />
+      <body className="font-body antialiased" suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -38,6 +38,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
